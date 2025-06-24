10b3e0f53da5d62ce629db74f19330042d3f3107
nitunwa <nitun.2@gmail.com>
2025-06-24 15:16:46 -0700

    initial set up

---
+++ src/main/java/com/example/testException/demoTestException/DemoTestExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.testException.demoTestException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestExceptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testException/demoTestException/DemoTestExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testException.demoTestException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
